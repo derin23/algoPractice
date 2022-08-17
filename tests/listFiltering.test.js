fee5b9a9252abcbf6893c2ee7f26bca02f2cb42c
derin <derinbino@gmail.com>
2022-08-17 16:41:47 -0500

    add[Aadding test file

--- tests/listFiltering.test.js
+++ tests/listFiltering.test.js
@@ -1,13 +1,13 @@
 const filter_list_test = require("../codewars/listFiltering.js")
 
 test("tests only numbers are returned", () => {
-    expect(filter_list_test(["a","n",1,2,3])).toBe([1,2,3]);
+    expect(filter_list_test(["a","n",1,2,3])).toEqual([1,2,3]);
 })
 
 test("test that checks result when no number is passed in", () => {
-    expect(filter_list_test(["a","b","c"]).toBe("no numbers in array!"));
+    expect(filter_list_test(["a","b","c"]).toEqual("no numbers in array!"));
 })
 
 test("test what happens if an empty array is passed in", () =>{
-    expect(filter_list_test([]).toBe(["no numbers in array!"]));
+    expect(filter_list_test([]).toBe("no numbers in array!"));
 })
