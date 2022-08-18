5ecb0d0d81ffecef6f77e3f7b9f2f659f1666ec9
derin <derinbino@gmail.com>
2022-08-18 15:01:48 -0500

    0(n) solution

--- codewars/switcheroo.js
+++ codewars/switcheroo.js
@@ -12,13 +12,11 @@
 //3) loop through the array variable from back to front and push items to final string
 //4) join the final string and return it
 function switcheroo(x){
-let stringItems = x.split("").reverse();
-console.log("stringItems : "+stringItems);
+let stringItems = x.split("");
 let stringArray = [];
 let finalString ="";
 
-for(let i = stringItems.length - 1; i <= 0; i --){
-    console.log(stringItems);
+for(let i = stringItems.length - 1; i >= 0; i --){
     stringArray.push(stringItems[i]);
 } 
 finalString = stringArray.join("");
@@ -26,5 +24,5 @@ return finalString;
 
 }
 
-console.log(switcheroo("abc"));
+console.log(switcheroo("abcasasa"));
 module.exports = switcheroo;
