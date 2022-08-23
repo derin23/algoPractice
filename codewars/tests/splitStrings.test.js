ba0ad70d59fe54c08988b82d54ad429f8a0306c3
derin <derinbino@gmail.com>
2022-08-22 19:41:27 -0500

    added odd number of input happy path

--- codewars/tests/splitStrings.test.js
+++ codewars/tests/splitStrings.test.js
@@ -3,3 +3,7 @@ const splitStringsTest = require("../splitString");
 test("testing normal happy path", () =>{
     expect(splitStringsTest("abcd")).toBe(["ab","cd"]);
 })
+
+test("testing odd number of inputs happy path", () => {
+    expect(splitStringsTest("abc")).toBe(["ab","c_"]);
+})
