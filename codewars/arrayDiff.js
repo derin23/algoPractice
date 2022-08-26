5ef3fc868b89ce6856e2b0cc489a8289a36a5bba
derin <derinbino@gmail.com>
2022-08-25 19:54:08 -0500

    solution and unit tests

--- codewars/arrayDiff.js
+++ codewars/arrayDiff.js
@@ -9,9 +9,24 @@
 // arrayDiff([1,2,2,2,3],[2]) == [1,3]
 
 //steps
-//1)create answer array
-function arrayDiff(a, b) {
-  
+/**
+ * 1. create an empty array named answer. We will store the the non-matching numbers in this variable
+ * 2.parse through array b
+ * 3.parse through array a. if match is found, do nothing. If match is not found, push the current value of array a to answer array.
+ * 4. return answer array 
+ */
+ function arrayDiff(a,b) {
+    let answer = a;
+    for(let i = 0; i < a.length; i++){
+        for(let j = 0; j < b.length; j++){
+            if(b[i] == a[j]){
+                answer.splice(i,1);
+            }
+        }      
+    }
+    return answer;
 }
 
 module.exports = arrayDiff;
+
+console.log(arrayDiff([1,2,3],[1,2])); //[3]
--- codewars/tests/arrayDiff.test.js
+++ codewars/tests/arrayDiff.test.js
@@ -1,9 +1,28 @@
 const arrayDiffTest = require("../arrayDiff");
 
 test("Testing normal happy path",() =>{
-    expect(arrayDiffTest([1,2],[1])).toBe([2]);
+    expect(arrayDiffTest([1,2],[1])).toEqual([2]);
 })
 
 test("Testing long happy path",() =>{
-    expect(arrayDiffTest([1,2,2,2,3],[1])).toBe([1,3]);
+    expect(arrayDiffTest([1,2,2,2,3],[2])).toEqual([1,3]);
+})
+
+test("([1,2], [1]) should return [2]", () => {
+    expect(arrayDiffTest([1,2],[1])).toEqual([2]);
+})
+test("([1,2,2], [1]) should return [2,2]",() => {
+    expect(arrayDiffTest([1,2,2],[1])).toEqual([2,2]);
+})
+test("([1,2,2],[2]) should return [1]",() => {
+    expect(arrayDiffTest([1,2,2], [2])).toEqual([1]);
+})
+test("(1,2,2),[] should return [1,2,2]",() => {
+    expect(arrayDiffTest([1,2,2],[])).toEqual([1,2,2]);
+})
+test("([], [1,2]) should return []", () => {
+    expect(arrayDiffTest([],[1,2])).toEqual([]);
+})
+test("([1,2,3], [1,2]) should return [2]", () => {
+    expect(arrayDiffTest([1,2,3], [1,2])).toEqual([3]);
 })
