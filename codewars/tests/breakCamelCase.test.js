79944c7e13202feea416ec519a4fe7be6f72e747
derin <derinbino@gmail.com>
2022-09-01 19:46:22 -0500

    more tests for camel case solution

--- codewars/tests/breakCamelCase.test.js
+++ codewars/tests/breakCamelCase.test.js
@@ -4,6 +4,19 @@ test("happy path", () => {
     expect(camelCaseTest.solution('camelCasing')).toBe('camel Casing');
 });
 
-// test("Testing long happy path",() =>{
-//     expect(camelCaseTest('camelCasingTest')).toBe('camel Casing Test');
-// });
+test("Testing long happy path",() =>{
+    expect(camelCaseTest.solution('camelCasingTest')).toBe('camel Casing Test');
+});
+
+test("Testing empty string",() =>{
+    expect(camelCaseTest.solution('')).toBe('');
+});
+
+test("Testing undefined",() =>{
+    expect(camelCaseTest.solution(undefined)).toBe("input is not correct");
+});
+
+test("Testing null",() =>{
+    expect(camelCaseTest.solution(null)).toBe("input is not correct");
+});
+
