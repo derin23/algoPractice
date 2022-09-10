0f77c605f965b461cf43727ae69146fb4f5af3bb
Derin Joseph <derin@Derins-MacBook-Pro.local>
2022-09-09 20:42:39 -0500

    fixed a typo

--- codewars/tests/simplePigLatin.test.js
+++ codewars/tests/simplePigLatin.test.js
@@ -1,5 +1,9 @@
 const simplePigLatinTest = require("../simplePigLatin");
 
 test('Pig latin is cool should return igPay atinlay siay oolcay', () => { 
-    expect(simplePigLatinTest.pigIt.pigIt("Pig latin is cool")).toBe("gPay atinlay siay oolcay");
+    expect(simplePigLatinTest.pigIt("Pig latin is cool")).toBe("igPay atinlay siay oolcay");
+})
+
+test('Hello world ! should return elloHay orldway !', () => {
+    expect(simplePigLatinTest.pigIt("Hello world !")).toBe("elloHay orldway !");
 })
