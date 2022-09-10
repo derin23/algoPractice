3e75481e2fe306a956c241870905d7e9c4dad1a9
Derin Joseph <derin@Derins-MacBook-Pro.local>
2022-09-09 20:08:24 -0500

    first test written

--- codewars/tests/simplePigLatin.test.js
+++ codewars/tests/simplePigLatin.test.js
@@ -1,1 +1,5 @@
 const simplePigLatinTest = require("../simplePigLatin");
+
+test('Pig latin is cool should return igPay atinlay siay oolcay', () => { 
+    expect(simplePigLatinTest.pigIt.pigIt("Pig latin is cool")).toBe("gPay atinlay siay oolcay");
+})
