19388d8fa9f8dbce5770c0732e595a94dc3ca9d7
derin <derinbino@gmail.com>
2022-09-14 19:57:54 -0500

    got one test working

--- codewars/tests/movingZerosToTheEnd.test.js
+++ codewars/tests/movingZerosToTheEnd.test.js
@@ -1,5 +1,5 @@
 const moveZerosTest = require("../movingZerosToTheEnd");
 
 test('[1,2,0,1,0,1,0,3,0,1] should return [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]', () => { 
-    expect(moveZerosTest.moveZeros([1,2,0,1,0,1,0,3,0,1])).toBe([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+    expect(moveZerosTest.moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])).toEqual([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
 })
