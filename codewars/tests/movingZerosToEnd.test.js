e13c89932b6c4409d1678b9bfd9acd458a967d8d
derin <derinbino@gmail.com>
2022-09-14 19:41:56 -0500

    test added

---
+++ codewars/tests/movingZerosToEnd.test.js
@@ -0,0 +1,5 @@
+const moveZerosTest = require("../movingZerosToTheEnd");
+
+test('[1,2,0,1,0,1,0,3,0,1] should return [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]', () => { 
+    expect(moveZerosTest.moveZeros([1,2,0,1,0,1,0,3,0,1])).toBe([1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+})
