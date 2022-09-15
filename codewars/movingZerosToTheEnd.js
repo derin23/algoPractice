45f45ca741bbd48b0a233ce555722b991e0c26e9
derin <derinbino@gmail.com>
2022-09-15 18:46:45 -0500

    solution

--- codewars/movingZerosToTheEnd.js
+++ codewars/movingZerosToTheEnd.js
@@ -3,12 +3,29 @@
 // moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 //steps
-//
+//1)create answer array
+//2)parse through array and check for 0
+//3)if we find 0 slice it out and push to end of array
+//4)return answer
 function moveZeros(arr) {
+    let answer = [];
+    let zeroCounter = 0;
+    for(let i = 0; i <arr.length; i++){
+        if(arr[i] === 0){
+            zeroCounter ++
+        }else{
+            answer.push(arr[i]);
+        }
+    }
+
+    for(let j = 0; j < zeroCounter; j++){
+        answer.push(0);
+    }
     return answer;
 }
+
+console.log(moveZeros([1,0,1,2,0,2,0,1,3,'a']));
 
 module.exports = {
     moveZeros : moveZeros
 };
-  
