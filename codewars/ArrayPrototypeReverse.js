066b5fc59f56780840631feb82291df5219230ca
derin <derinbino@gmail.com>
2022-09-18 23:31:16 -0500

    done!

--- codewars/ArrayPrototypeReverse.js
+++ codewars/ArrayPrototypeReverse.js
@@ -15,17 +15,16 @@
 //3) loop through local array
 //4) take last item from the array and push to answer array
 //4) return answer array
-Array.prototype.reverse = function() { 
-    let localArray = this;
+Array.prototype.reverse = function() { //time complexity o(n) because of the for loop, space complexity: o(n) worst case because I am creating a local variable answer
     let answer = [];
 
-    for(let i = localArray.length -1; i >= 0; i--){
+    for(let i = this.length -1; i >= 0; i--){
         answer.push(this[i]);
     }
     return answer;
   };
 
-let x = [];
+let x = [1,2,3];
 console.log(x.reverse());
 
 module.exports = Array.prototype.reverse;
