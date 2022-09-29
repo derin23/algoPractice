7ffd7c44149550ed2675045a93dab049081e5b07
derin <derinbino@gmail.com>
2022-09-29 18:56:31 -0500

    doing longest common again

--- leetcode/tests/longestCommonPrefix.test.js
+++ leetcode/tests/longestCommonPrefix.test.js
@@ -1,5 +1,11 @@
 const longestCommonPrefixTest = require("../longestCommonPrefix");
+const longestCommonPrefixTest1 = require("../longestCommonPrefix1");
 
 test(' ["flower","flow","flight"] should return "fl"', () => { 
     expect(longestCommonPrefixTest.longestCommonPrefix(["flower","flow","flight"])).toBe("fl");
+ })
+
+ 
+test(' ["flower","flow","flight"] should return "fl"', () => { 
+    expect(longestCommonPrefixTest1.longestCommonPrefix(["flower","flow","flight"])).toBe("fl");
  })
