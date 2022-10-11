0cbd80351a9719f6b1caaed0cf147024aeb56e03
derin <derinbino@gmail.com>
2022-10-10 23:18:25 -0500

    completed sum of 2 nums test"

--- binarySearch/tests/sumOfTwoNumbers.test.js
+++ binarySearch/tests/sumOfTwoNumbers.test.js
@@ -7,3 +7,10 @@ test("([1,2,3,4,5],9) should return true",() =>{
     expect(solutionTest.solve([1,2,3,4,5],9)).toBe(true);
 })
 
+test("([1,2,3,4,5],98) should return false",() =>{
+    expect(solutionTest.solve([1,2,3,4,5],98)).toBe(false);
+})
+
+test("([1,2,3,4,5],98) should return false for kings solution",() =>{
+    expect(solutionTest.solveKing([1,2,3,4,5],98)).toBe(false);
+})
