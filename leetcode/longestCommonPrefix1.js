7124a1f681c88a35414b35fd7ca093058c396c54
derin <derinbino@gmail.com>
2022-10-16 21:02:49 -0500

    practicing longest common prefix

--- leetcode/longestCommonPrefix1.js
+++ leetcode/longestCommonPrefix1.js
@@ -37,7 +37,7 @@ var longestCommonPrefix = function(strs) {
     let answer = strs[0];
     console.log("answer : ",answer);
     for(let i = 1; i < strs.length; i++){
-        while(answer !== strs[i]){
+        while(strs[i].indexOf(answer) !==0){
             answer = answer.substring(0, answer.length - 1);
             console.log("answer2 ",answer);
             console.log(strs[i]);
