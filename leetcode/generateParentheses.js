e03a70e779a68696d45da0176b30acfa61803da4
derin <derinbino@gmail.com>
2022-11-21 17:44:45 -0600

    new prob

--- leetcode/generateParentheses.js
+++ leetcode/generateParentheses.js
@@ -29,9 +29,29 @@
 //steps
 //1)
 var generateParenthesis = function(n) {
-    return ["((()))","(()())","(())()","()(())","()()()"];
+    const solution = [];
+
+    const generateCombo = (leftPCount, rightPCount, partial)=>{
+        if(leftPCount > rightPCount) return;
+        if(!leftPCount && !rightPCount) {
+            solution.push(partial);
+            console.log('pcounts are == 0: ', partial);
+        }
+        if(leftPCount > 0) {
+            generateCombo (leftPCount - 1, rightPCount, partial + '(');
+            console.log('leftPCount > 0: ', partial);
+        }
+        if(rightPCount > 0) {
+            generateCombo(leftPCount, rightPCount - 1, partial + ")");
+            console.log('rightPCount > 0: ', partial);
+        }
+    }
+    generateCombo(n,n,'');
+    return solution;
 };
 
 module.exports = {
     generateParenthesis : generateParenthesis
 }
+
+console.log(generateParenthesis(3));
