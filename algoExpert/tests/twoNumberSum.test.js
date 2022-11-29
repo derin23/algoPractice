9275191614dbbd4e54eb67f90ba5ba55c052549b
derin <derinbino@gmail.com>
2022-11-28 19:11:18 -0600

    answer

--- algoExpert/tests/twoNumberSum.test.js
+++ algoExpert/tests/twoNumberSum.test.js
@@ -1,5 +1,5 @@
 const twoNumberSumTest = require('../twoNumberSum');
 
 test('expect [11,-1]', () => {
-    expect(twoNumberSumTest.twoNumberSum([3,5,4,8,11,1,-1,6], 10)).toEqual([11, -1])
+    expect(twoNumberSumTest.twoNumberSum([3,5,4,8,11,1,-1,6], 10)).toEqual([-1, 11])
 })
--- algoExpert/twoNumberSum.js
+++ algoExpert/twoNumberSum.js
@@ -1,5 +1,25 @@
 function twoNumberSum(array, targetSum) {
     
+  array.sort(function (a,b) {
+    return a - b; // Ascending  
+    });
+  
+
+     let left = 0;
+     let right = array.length - 1;
+
+     while(left <= right){
+      let currentSum = array[left] + array[right];
+      if( currentSum == targetSum){
+        return [array[left],array[right]];
+      }else if(currentSum < targetSum){
+        left ++;
+      }else{
+        right --;
+      }
+     }
+
+     return [];
      
   }
 
