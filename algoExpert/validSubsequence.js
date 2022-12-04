cc97663d3e19c8fdddd6e2fbd3480d18a26ca704
derin <derinbino@gmail.com>
2022-12-03 19:35:28 -0600

    [A[BWIP

--- algoExpert/validSubsequence.js
+++ algoExpert/validSubsequence.js
@@ -21,4 +21,18 @@ function isValidSubsequence(array, sequence) {
     isValidSubsequence : isValidSubsequence
   }
 
-  console.log(isValidSubsequence([1,2,3,4,5],[2,1]));
+ // console.log(isValidSubsequence([1,2,3,4,5],[2,1]));
+
+  function isValidSubsequence1(array, sequence) {
+    let map = {}
+    for(let i = 0; i < array.length; i++){
+      map[i] = array[i];
+    }  
+    
+    for(let j = 0; j < sequence.length; j++){
+        if(Object.values(map).includes(sequence[j])){
+          return true
+        }else return false;
+    }
+  }
+  console.log(isValidSubsequence1([1, 1, 1, 1, 1],[1,1,1]));
