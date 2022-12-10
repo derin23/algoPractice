2748ed4da7c41f3ad621a666bf2dda61faf1a8ee
derin <derinbino@gmail.com>
2022-12-09 19:39:55 -0600

    optimal solution

--- algoExpert/sortedSquaredArray.js
+++ algoExpert/sortedSquaredArray.js
@@ -1,15 +1,42 @@
-function sortedSquaredArray(array) {
+function sortedSquaredArrayBrute(array) {
     // Write your code here.
     let answer = [];
     for(let i = 0; i < array.length; i++){
         answer.push((array[i]) * (array[i]));
     }
     return answer.sort((a, b) => a - b);
-  }
+}
+
+
+function sortedSquaredArray(array) {
+    // Write your code here.
+    let answer = [];
+
+    let start = 0;
+    let end = array.length - 1;
+
+    while(start <= end){
+        
+        let smallNum = array[start];
+        let largeNum = array[end];
+
+        if(Math.abs(smallNum) < Math.abs(largeNum)){
+            answer.unshift(largeNum * largeNum);
+            end--;
+        }else{
+            answer.unshift(smallNum * smallNum);
+            start++;
+        }
+    }
+    
+    return answer;
+
+}
 
 
 module.exports = {
+    sortedSquaredArrayBrute: sortedSquaredArrayBrute,
     sortedSquaredArray: sortedSquaredArray
 }
 
-console.log(sortedSquaredArray([1,2,3,5,6,8,9]));
+console.log(sortedSquaredArray([-7,-5,-4,3,6,8,9]));
--- algoExpert/tests/sortedSquaredArray.test.js
+++ algoExpert/tests/sortedSquaredArray.test.js
@@ -1,9 +1,17 @@
 const sortedSquaredArrayTest = require('../sortedSquaredArray');
 
 test('expected return [1, 4, 9, 25, 36, 64, 81]', () => {
-    expect(sortedSquaredArrayTest.sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])).toEqual([1, 4, 9, 25, 36, 64, 81]);
+    expect(sortedSquaredArrayTest.sortedSquaredArrayBrute([1, 2, 3, 5, 6, 8, 9])).toEqual([1, 4, 9, 25, 36, 64, 81]);
+})
+
+test('expected return [1, 4, 9, 16, 25]', () => {
+    expect(sortedSquaredArrayTest.sortedSquaredArrayBrute([-5, -4, -3, -2, -1])).toEqual([1, 4, 9, 16, 25]);
 })
 
 test('expected return [1, 4, 9, 25, 36, 64, 81]', () => {
+    expect(sortedSquaredArrayTest.sortedSquaredArray([1, 2, 3, 5, 6, 8, 9])).toEqual([1, 4, 9, 25, 36, 64, 81]);
+})
+
+test('expected return [1, 4, 9, 16, 25]', () => {
     expect(sortedSquaredArrayTest.sortedSquaredArray([-5, -4, -3, -2, -1])).toEqual([1, 4, 9, 16, 25]);
 })
