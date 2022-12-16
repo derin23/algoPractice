13e2dd52d980731d37b7a2ec9736212b936cf302
derin <derinjoseph23@gmail.com>
2022-12-15 23:24:43 -0600

    made test work

--- algoExpert/tests/tournamentWinner.test.js
+++ algoExpert/tests/tournamentWinner.test.js
@@ -1,11 +1,11 @@
 const tournamentWinnerTest = require("../tournamentWinner");
 
-let competitions = [
-    ["HTML", "C#"],
-    ["C#", "Python"],
-    ["Python", "HTML"],
-  ];
-let results = [0,0,1]
+// let competitions = [
+//     ["HTML", "C#"],
+//     ["C#", "Python"],
+//     ["Python", "HTML"],
+//   ];
+// let results = [0,0,1]
 test("should return Python",()=>{
     expect(tournamentWinnerTest.tournamentWinner([["HTML", "C#"],["C#", "Python"],["Python", "HTML"]],[0,0,1])).toBe("Python");
 })
