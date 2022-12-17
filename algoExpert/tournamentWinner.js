79f0885bb60af63b56dd9d1ee980688769244839
Derin Joseph <43301427+derin23@users.noreply.github.com>
2022-12-16 23:29:47 -0600

    Update tournamentWinner.js

--- algoExpert/tournamentWinner.js
+++ algoExpert/tournamentWinner.js
@@ -1,7 +1,7 @@
 function tournamentWinner(competitions, results) {
     // Write your code here.
     // Do not understand the question fully yet
-    testing github
+    // testing github (not working)
     return "";
   }
 
