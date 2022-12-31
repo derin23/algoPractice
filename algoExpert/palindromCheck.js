bb03b79898e1e2e502c18d939333d2fcae830a3a
derin <derinjoseph23@gmail.com>
2022-12-30 23:56:45 -0600

    WIP recursion

--- algoExpert/palindromCheck.js
+++ algoExpert/palindromCheck.js
@@ -10,4 +10,25 @@ function isPalindrome(string) {
     return false;
   }
 
-  console.log(isPalindrome("abcdcba"));
+  //console.log(isPalindrome("abcdcba"));
+
+  function isPalindrome1(string) {
+    // Write your code here.
+    let localArray = [];
+    for(let i = string.length-1; i >= 0; i--){
+        localArray.push(string[i]);
+    }
+    if (string == localArray.join("")){
+        return true;
+    }
+    return false;
+  }
+
+  //console.log(isPalindrome1("abcdcba"));
+
+  function isPalindromeRecursion(string) {
+    // Write your code here.
+
+  }
+
+  //console.log(isPalindrome1("abcdcba"));
