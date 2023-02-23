6df1bc8bb3fd6a50056a5f5ef9529f88a7a1e94c
derin <derinjoseph23@gmail.com>
2023-02-22 23:58:23 -0600

    power of three

---
+++ leetcode/powerOfThree.js
@@ -0,0 +1,54 @@
+// Link to original problem: https://leetcode.com/problems/power-of-three/
+
+// 326. Power of Three
+// Easy
+// 2.5K
+// 237
+// company
+// Google
+// company
+// Bloomberg
+// company
+// Apple
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+// Example 1:
+
+// Input: n = 27
+// Output: true
+// Explanation: 27 = 33
+// Example 2:
+
+// Input: n = 0
+// Output: false
+// Explanation: There is no x where 3x = 0.
+// Example 3:
+
+// Input: n = -1
+// Output: false
+// Explanation: There is no x where 3x = (-1).
+ 
+
+// Constraints:
+
+// -231 <= n <= 231 - 1
+ 
+
+// Follow up: Could you solve it without loops/recursion?
+
+var isPowerOfThree = function(n) {
+    //base case
+    if(n < 1){
+        return false
+    }
+
+    if(n%3 == 0){
+       return isPowerOfThree(n/=3);
+    }
+
+    return n == 1;
+};
---
+++ leetcode/powerOfThree.py
@@ -0,0 +1,56 @@
+# // Link to original problem: https://leetcode.com/problems/power-of-three/
+
+# // 326. Power of Three
+# // Easy
+# // 2.5K
+# // 237
+# // company
+# // Google
+# // company
+# // Bloomberg
+# // company
+# // Apple
+# // Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+# // An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ 
+
+# // Example 1:
+
+# // Input: n = 27
+# // Output: true
+# // Explanation: 27 = 33
+# // Example 2:
+
+# // Input: n = 0
+# // Output: false
+# // Explanation: There is no x where 3x = 0.
+# // Example 3:
+
+# // Input: n = -1
+# // Output: false
+# // Explanation: There is no x where 3x = (-1).
+ 
+
+# // Constraints:
+
+# // -231 <= n <= 231 - 1
+ 
+
+# // Follow up: Could you solve it without loops/recursion?
+
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        #base case
+        if(n < 1):
+            return False
+        
+
+        if(n%3 == 0):
+            return self.isPowerOfThree(n = n/3)
+        return n == 1
+
+#ask King how to call class methods
+solution = Solution
+print(solution.isPowerOfThree(27))
