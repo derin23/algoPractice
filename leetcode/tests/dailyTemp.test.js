b45b46f3152ca49e675003fd0a388e188d3d1a0d
derin <derinjoseph23@gmail.com>
2023-03-09 20:56:53 -0600

    jest tests

--- leetcode/tests/dailyTemp.test.js
+++ leetcode/tests/dailyTemp.test.js
@@ -1,5 +1,5 @@
 const dailyTemperaturesTest = require("../dailyTemp");
 
-test("",()=>{
+test("[73,74,75,71,69,72,76,73] returns [1,1,4,2,1,1,0,0]",()=>{
     expect(dailyTemperaturesTest.dailyTemperatures([73,74,75,71,69,72,76,73])).toEqual([1,1,4,2,1,1,0,0]);
 })
