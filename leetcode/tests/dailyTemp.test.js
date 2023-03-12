112132c5e5baed9533f44bb3c1ac7adb707977ff
derin <derinjoseph23@gmail.com>
2023-03-11 21:42:14 -0600

    more tests

--- leetcode/tests/dailyTemp.test.js
+++ leetcode/tests/dailyTemp.test.js
@@ -7,3 +7,8 @@ test("[73,74,75,71,69,72,76,73] returns [1,1,4,2,1,1,0,0]",()=>{
 test("[30,40,50,60] returns [1,1,1,0]",()=>{
     expect(dailyTemperaturesTest.dailyTemperatures([30,40,50,60] )).toEqual([1,1,1,0]);
 })
+
+
+test("[30] returns [0]",()=>{
+    expect(dailyTemperaturesTest.dailyTemperatures([30])).toEqual([0]);
+})
