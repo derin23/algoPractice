286bc8a07cd75be9675171ed35b9b697d103472f
derin <derinjoseph23@gmail.com>
2023-04-12 20:10:17 -0500

    peeking iterator

--- leetcode/peekingIterator.js
+++ leetcode/peekingIterator.js
@@ -51,3 +51,60 @@ At most 1000 calls will be made to next, hasNext, and peek.
 Follow up: How would you extend your design to be generic and work with all types, not just integer?
 
 */
+
+/**
+ * // This is the Iterator's API interface.
+ * // You should not implement it, or speculate about its implementation.
+ * function Iterator() {
+ *    @ return {number}
+ *    this.next = function() { // return the next number of the iterator
+ *       ...
+ *    }; 
+ *
+ *    @return {boolean}
+ *    this.hasNext = function() { // return true if it still has numbers
+ *       ...
+ *    };
+ * };
+ */
+
+/**
+ * @param {Iterator} iterator
+ */
+var PeekingIterator = function(iterator) {
+   
+    this.iterator = iterator;
+    this.nextVal = this.iterator.hasNext() ? this.iterator.next() : undefined;
+  
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.peek = function() {
+   return this.nextVal;
+};
+
+/**
+ * @return {number}
+ */
+PeekingIterator.prototype.next = function() {
+    const temp = this.nextVal;
+    this.nextVal = this.iterator.hasNext() ? this.iterator.next() : undefined;
+    return temp;
+};
+
+/**
+ * @return {boolean}
+ */
+PeekingIterator.prototype.hasNext = function() {
+    return this.nextVal !== undefined;
+};
+
+/** 
+ * Your PeekingIterator object will be instantiated and called as such:
+ * var obj = new PeekingIterator(arr)
+ * var param_1 = obj.peek()
+ * var param_2 = obj.next()
+ * var param_3 = obj.hasNext()
+ */
