196e85160a0b9fef34b18cda3112870a55de19b6
derin <derinjoseph23@gmail.com>
2023-04-22 21:33:44 -0500

    WIP

---
+++ algoExpert/commonCharacters.js
@@ -0,0 +1,24 @@
+//Link to problem: https://www.algoexpert.io/questions/common-characters
+
+function commonCharacters(strings) {
+    // Write your code here.
+    let alph = {
+        "a":false, "b":false, "c":false, "d":false, "e":false, "f":false, "g":false, "h":false, "i":false, "k":false, "l":false, "m":false, "n":false, "o":false, "p":false, "q":false, "r": false, "s":false, "t":false, "u":false, "v":false, "w":false, "x":false, "y":false, "z":false
+    }
+
+    let seenLetters = new Map();
+
+    for(let element of strings){
+        let uniqueStringChars = new Set(element);
+        for(char of uniqueStringChars){
+            if(!char in seenLetters){
+                seenLetters[char] = 0;
+            }
+            seenLetters[char] += 1;
+        }
+    }
+
+    return seenLetters;
+  }
+  
+  console.log(commonCharacters(["abc", "bcd", "cbaccd"]));
