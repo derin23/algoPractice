c549416d8b0c344c0401c15d78c61417c8c3cec0
derin <derinjoseph23@gmail.com>
2023-05-24 20:56:25 -0500

    merge two lists recursion solution

--- leetcode/mergeTwoSortedLists.js
+++ leetcode/mergeTwoSortedLists.js
@@ -53,12 +53,17 @@ Both list1 and list2 are sorted in non-decreasing order.
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    let result = new ListNode()
-    for(item in list1){
-        for(node in list2){
-            if(item < node){
-                result
-            }
-        }
+    if (!l1) return l2;
+    else if (!l2) return l1;
+    else if (l1.val <= l2.val) {
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    } else {
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2
     }
 };
+
+module.exports = {
+    mergeTwoLists : mergeTwoLists
+}
