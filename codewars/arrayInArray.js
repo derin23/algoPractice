1800fa5f502cffdb07edf1088d1e64ee26057963
derin <derinjoseph23@gmail.com>
2024-01-02 00:49:22 -0600

    finsihed algo

--- codewars/arrayInArray.js
+++ codewars/arrayInArray.js
@@ -8,9 +8,20 @@
 // [[1],[2]] => true
 // ['1','2'] => false
 // [{1:1},{2:2}] => false
-const arrCheck = value =>
-function arrayCheck(value) {
-return true;
+// const arrCheck = value =>
+function arrCheck(value) {
+  if(value == null || value == undefined || value == ""){
+    return false
+  }  
+  let count = 0;
+    for(let i = 0; i < value.length; i++){
+      if(Array.isArray(value[i]) == true) {count++}  
+    }
+    if(count == value.length){;
+      return true;
+    }
+  return false;
 }
 
-module.exports = arrayCheck;
+console.log(arrCheck([[2],[1]]));
+module.exports = arrCheck;
