f1bd12d7a7e3a0a7f2a7a3f151a69ebac2ce9c06
derin <derinjoseph23@gmail.com>
2024-01-02 00:48:13 -0600

    finished problem and tests

--- codewars/tests/arrayInArray.test.js
+++ codewars/tests/arrayInArray.test.js
@@ -1,5 +1,17 @@
-const arrayTest = require("../arrayTest");
+const arrayTest = require("../arrayInArray");
 
 test("Testing normal happy path",() =>{
-    expect(arrayTest([1,2],[1])).toEqual(true);
+    expect(arrayTest([[2],[1]])).toBe(true);
+})
+
+test("Testing null",() =>{
+    expect(arrayTest(null)).toBe(false);
+})
+
+test("Testing empty input",() =>{
+    expect(arrayTest()).toBe(false);
+})
+
+test("Testing not an array in an array",() =>{
+    expect(arrayTest(["",""])).toBe(false);
 })
