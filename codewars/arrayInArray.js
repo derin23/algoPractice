905d1391d2b2646a2b046d9263efee6dcc02af47
derin <derinjoseph23@gmail.com>
2024-04-27 14:13:06 -0500

    new prob

--- codewars/arrayInArray.js
+++ codewars/arrayInArray.js
@@ -1,3 +1,4 @@
+//Link: https://www.codewars.com/kata/582c81d982a0a65424000201/javascript
 // DESCRIPTION:
 // Is every value in the array an array?
 
---
+++ leetcode/wordEqualsSummation.js
@@ -0,0 +1,7 @@
+//Link: https://leetcode.com/problems/check-if-word-equals-summation-of-two-words/
+
+//Steps
+//1) create a map with each letter mapped to its numerical value
+//2) convert word to number value
+//3) check if the number values add up to target value
+
